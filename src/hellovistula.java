165551ecb741e53bd05f9182602ff40ef1158c33
panashe <panashemusengi58@gmail.com>
2023-10-12 10:51:57 +0100

    tabvisa line

--- src/hellovistula.java
+++ src/hellovistula.java
@@ -3,7 +3,7 @@ public class hellovistula {
         System.out.println("hello vistula");
         System.out.println("hello vistula");
         System.out.println("hello vist5ul221a");
-        System.out.println("hello vistkjbdfvjhvbula");
+
 
 
 
