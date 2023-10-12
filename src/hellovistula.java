3c2921e471425244989aa3ee2c35d48bb679dd8c
panashe <panashemusengi58@gmail.com>
2023-10-12 10:08:34 +0100

    sup

---
+++ src/hellovistula.java
@@ -0,0 +1,8 @@
+public class hellovistula {
+    public static void main(String[] args) {
+        System.out.println("hello vistula");
+        System.out.println("hello vistula");
+        System.out.println("hello vist5ul221a");
+
+    }
+}
